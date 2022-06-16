623f77e3c3c3e6aa27ab93ea32178416c6716c68
Jazz Haddlesey <jazzhenry@hotmail.co.uk>
2022-06-16 10:01:26 +0000

    JH: updated build.sh

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from os import getenv
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] =  "sqlite:///data.db" #os.getenv("DATABASE_URI")
-app.config['SECRET_KEY'] = 'sceret key'
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
