60faac28c8ed5829c403130c59d14360625103fc
Jazz Haddlesey <jazzhenry@hotmail.co.uk>
2022-06-17 15:22:32 +0000

    JH: debugging

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 echo "Killing existing processes"
-sudo kill $(cat gunicornpidfile)
+# sudo kill $(cat gunicornpidfile)
 python3 create.py
 python3 -m gunicorn -D -b  0.0.0.0:5000 -w 4 application:app  -p gunicornpidfile 
