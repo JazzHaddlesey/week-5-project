0e95470484424ff90ee2ca22bb18e06ee138de31
Jazz Haddlesey <jazzhenry@hotmail.co.uk>
2022-06-17 14:44:29 +0000

    JH: updated deploy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 echo "Killing existing processes"
-sudo kill -9 $(lsof -i :5000 | cut -d' ' -f2 | tail -n 5 | head -n 1)
+sudo kill $(cat gunicornpidfile)
 python3 create.py
-BUILD_ID=nokill python3 -m gunicorn -D --workers 4 --bind 0.0.0.0:5000 app:app 
+python3 -m gunicorn -D -b  0.0.0.0:5000 -w 4 application:app  -p gunicornpidfile 
