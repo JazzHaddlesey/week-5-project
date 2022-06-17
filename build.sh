061359686a85eb4b018978817368fd151c5c8c3e
Jazz Haddlesey <jazzhenry@hotmail.co.uk>
2022-06-17 15:17:29 +0000

    JH: debugging

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 echo "Installing packages"
 sudo apt install python3 python3-pip python3-venv -y
 echo "Setting up venv"
-pyhton -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 echo "Installing dependencies"
 pip3 install -r requirements.txt
